38862f3e86d2e03cfe70493b2d5de1b5ec630b00
Aiden Luo <aiden0xz@gmail.com>
2018-12-03 15:24:42 +0800

    Add leetcode 028,029 solutions

---
+++ leetcode/028_implement_strStr.py
@@ -0,0 +1,81 @@
+"""Implement strStr()
+
+Imporment strStr()
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of
+haystack.
+
+Example 1:
+
+    Input: haystack = "hello", needle = "ll
+    Ouput: 2
+
+Example 2:
+
+    Input: haystack = "aaaaa", needle = "bba"
+    Output: -1
+
+Clarification:
+
+What should we return when needle is an empty string? This is a greate question to ask during an
+interview.
+
+For the purpose of this problem, we will return 0 when dle is an empty string. This is consistent 
+to C's strStr() and Java's indexOf().
+
+Reference https://leetcode.com/problems/implement-strstr/
+"""
+
+class Solution:
+    
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(needle) == 0:
+            return 0
+
+        if len(haystack) == 0:
+            return -1
+
+        i, n = 0, len(haystack)
+        while i < n:
+            if i + len(needle) > n:
+                return  -1
+
+            if haystack[i] != needle[0]:
+                i += 1
+                continue
+
+            found = True
+            for j in range(len(needle)):
+                if haystack[i + j] != needle[j]:
+                    found = False
+                    break
+
+            if not found:
+                i += 1
+                continue
+            else:
+                return i
+        return -1
+
+
+if __name__ == '__main__':
+    cases = [
+        ('hello', 'h', 0),
+        ('hello', 'ell', 1),
+        ('hello', 'll', 2),
+        ('aaaaa', 'bba', -1),
+        ('', 'a', -1),
+        ('aa', '', 0),
+    ]
+
+    solutions = [Solution]
+    for case in cases:
+        for s in solutions:
+            print(case, s().strStr(case[0], case[1]))
+            assert s().strStr(case[0], case[1]) == case[2]
+
---
+++ leetcode/029_divide_two_integers.py
@@ -0,0 +1,131 @@
+"""Divide Two Integers
+
+Given two integers dividend and dvisor, divide two integer without using multiplication, 
+dvision and mod perator.
+
+Return the quotient after dividing dividend by dvisor.
+
+The integer division should truncate toward zero.
+
+Example 1:
+
+    Input: dividend = 10, divisor = 3
+    Output: 3
+
+Example 2:
+
+    Input: divident = 7, divisor = -3
+    Output: -2
+
+Note:
+    * Both divident and divisor will be 32-bit signed integers.
+    * The divisor will never be 0.
+    * Assume we are dealing with the environment which could only sotre integers with 32-bit
+      singed integers range: [-2**31, 2**31-1]. For the purpose of this problem, assume that
+      you function returns 2**31 -1 when the division result overflows.
+
+Reference https://leetcode.com/problems/divide-two-integers/
+
+"""
+
+class Solution:
+    """每次2倍增长"""
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if divisor == 1:
+            return dividend
+
+        if divisor == -1:
+            result = -dividend
+            if result >= 2 ** 31 -1:
+                return 2 ** 31 - 1
+            else:
+                return result
+        if dividend < 0:
+            a = -dividend
+        else:
+            a = dividend
+        
+        if divisor < 0:
+            b = -divisor
+        else:
+            b = divisor
+
+        def divide(a, b):
+            if a < b:
+                return 0
+            x = b
+            result = 1
+
+            while a >= (x << 1):
+                x <<= 1
+                result <<= 1
+            
+            return divide(a - x, b) + result
+
+        result = divide(a, b)
+
+        if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0):
+            return result
+        else:
+            return -result
+
+
+
+class SolutionBasedSubtraction:
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if divisor == 1:
+            return dividend
+
+        if divisor == -1:
+            result = -dividend
+            if result >= 2 ** 31 -1:
+                return 2 ** 31 - 1
+            else:
+                return result
+
+        if dividend < 0:
+            a = -dividend
+        else:
+            a = dividend
+        
+        if divisor < 0:
+            b = -divisor
+        else:
+            b = divisor
+
+        result = 0
+        while a >= b:
+            a -= b
+            result += 1
+
+        if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0):
+            return result
+        else:
+            return -result
+
+
+if __name__ == '__main__':
+    cases = [
+        (10, 3, 3),
+        (7, -3, -2),
+        (-2147483648, -1, 2147483647),
+        (2147483647, 2, 1073741823)
+    ]
+
+    solutions = [Solution]
+
+    for case in cases:
+        for s in solutions:
+            result = s().divide(case[0], case[1])
+            print(case, result)
+            assert case[2] == result
