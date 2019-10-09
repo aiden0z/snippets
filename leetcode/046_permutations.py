13d66f8639a2f9451052ff33fe41391fd56c9f25
Aiden Luo <aiden0xz@gmail.com>
2019-10-10 00:11:19 +0800

    Add leetcode o46 solution

--- leetcode/046_permutations.py
+++ leetcode/046_permutations.py
@@ -21,17 +21,43 @@ from typing import List
 
 class Solution:
 
-    def cal(self, nums: List[int], result: []):
-        if len(nums) == 1:
-            return [nums]
-        for i in range(len(nums)):
-            value = [nums[i]]
-            left = nums[:i] + nums[i + 1:]
-            result.append(value + self.cal(left, result))
-        return result
+    def cal(self, nums: List[int], l: int, r: int, result: List):
+        nums = nums[:]
+        if l == r:
+            result.append(nums)
+            return
+        for i in range(l, r + 1):
+            nums[l], nums[i] = nums[i], nums[l]
+            self.cal(nums, l + 1, r, result)
+            # 进行回溯, why ？
+            nums[l], nums[i] = nums[i], nums[l]
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.cal(nums, [])
+        result = []
+        self.cal(nums, 0, len(nums) - 1, result)
+        return result
+
+
+class SolutionB:
+
+    # The idea is to one by one extract all elements,
+    # place them at first position and recur for remaining list.
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return []
+        if len(nums) == 1:
+            return [nums]
+
+        l = []
+
+        for i in range(len(nums)):
+            m = nums[i]
+            rem_nums = nums[:i] + nums[i + 1:]
+            # Generating all permutations where m is first element
+            for p in self.permute(rem_nums):
+                l.append([m] + p)
+
+        return l
 
 
 if __name__ == '__main__':
@@ -47,9 +73,48 @@ if __name__ == '__main__':
                 [3, 2, 1]
             ]
         ),
+        (
+            [1, 2],
+            [
+                [1, 2],
+                [2, 1]
+            ]
+        ),
+        (
+            [1, 2, 3, 4],
+            [
+                [1, 2, 3, 4],
+                [1, 2, 4, 3],
+                [1, 3, 2, 4],
+                [1, 3, 4, 2],
+                [1, 4, 2, 3],
+                [1, 4, 3, 2],
+                [2, 1, 3, 4],
+                [2, 1, 4, 3],
+                [2, 3, 1, 4],
+                [2, 3, 4, 1],
+                [2, 4, 1, 3],
+                [2, 4, 3, 1],
+                [3, 1, 2, 4],
+                [3, 1, 4, 2],
+                [3, 2, 1, 4],
+                [3, 2, 4, 1],
+                [3, 4, 1, 2],
+                [3, 4, 2, 1],
+                [4, 1, 2, 3],
+                [4, 1, 3, 2],
+                [4, 2, 1, 3],
+                [4, 2, 3, 1],
+                [4, 3, 1, 2],
+                [4, 3, 2, 1]
+            ]
+        )
+
     ]  # yapf: disable
 
     for case in cases:
-        for S in [Solution]:
-            pprint.pprint(S().permute(case[0]))
+        for S in [Solution, SolutionB]:
+            result = S().permute(case[0])
+            for item in case[1]:
+                assert item in result
             # assert S().permute(case[0]) == case[1]
