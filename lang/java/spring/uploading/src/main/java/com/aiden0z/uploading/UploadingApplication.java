69cfca004132ef666c1d415c6d89d65f2e67e20a
Aiden Luo <aiden0xz@gmail.com>
2022-05-19 23:44:39 +0800

    Add Spring uploading demo code

--- lang/java/spring/uploading/src/main/java/com/aiden0z/uploading/UploadingApplication.java
+++ lang/java/spring/uploading/src/main/java/com/aiden0z/uploading/UploadingApplication.java
@@ -1,9 +1,12 @@
 package com.aiden0z.uploading;
 
 import com.aiden0z.uploading.storage.StorageProperties;
+import com.aiden0z.uploading.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -13,4 +16,11 @@ public class UploadingApplication {
         SpringApplication.run(UploadingApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
--- lang/java/spring/uploading/src/main/java/com/aiden0z/uploading/storage/FileSystemStorageService.java
+++ lang/java/spring/uploading/src/main/java/com/aiden0z/uploading/storage/FileSystemStorageService.java
@@ -2,16 +2,20 @@ package com.aiden0z.uploading.storage;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class FileSystemStorageService implements StorageService {
@@ -20,7 +24,7 @@ public class FileSystemStorageService implements StorageService {
 
     @Autowired
     public FileSystemStorageService(StorageProperties properties) {
-        this.rootLocation = Path.get(properties.getLocation());
+        this.rootLocation = Paths.get(properties.getLocation());
     }
 
     @Override
@@ -30,12 +34,16 @@ public class FileSystemStorageService implements StorageService {
                 throw new StorageException("Failed to store empty file.");
             }
 
+            if (file.getOriginalFilename() == null) {
+                throw new StorageException("Failed to store empty file.");
+            }
+
             Path destinationFile = rootLocation.resolve(
                     Paths.get(file.getOriginalFilename())).normalize().toAbsolutePath();
 
             // Security check
             if(!destinationFile.getParent().equals(rootLocation.toAbsolutePath())) {
-                throw new StorageException("Cannot store file outside current directory.")
+                throw new StorageException("Cannot store file outside current directory.");
             }
 
             try (InputStream inputStream = file.getInputStream()) {
@@ -46,4 +54,48 @@ public class FileSystemStorageService implements StorageService {
         }
     }
 
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        Path file = load(filename);
+        try {
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            throw new StorageFileNotFoundException("Could not read file: " + filename);
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage.", e);
+        }
+    }
 }
