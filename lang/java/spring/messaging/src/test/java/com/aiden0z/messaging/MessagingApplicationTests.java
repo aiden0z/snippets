b1d673b1917ae12a085ae0811eccffe34992bcc2
Aiden Luo <aiden0xz@gmail.com>
2022-05-25 00:16:26 +0800

    Add Spring redis messaging demo project

---
+++ lang/java/spring/messaging/src/test/java/com/aiden0z/messaging/MessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiden0z.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
